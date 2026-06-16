95f5a290642c05fc4b962a934438a4ef4e70857f
Adil <batyrbekadil2008@gmail.com>
2026-06-16 22:50:13 +0500

    Add files via upload

---
+++ send.php
@@ -0,0 +1,17 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$file= fopen("messages.txt", "a");
+fwrite($file,
+"Имя: $name | Email: $email | Сообщение: $message\n"
+);
+fclose($file);
+
+echo "<h2>Сообщение отправлено!</h2>";
+echo "<a href='Web.html'>Вернуться назад</a>";
+
+
+
+?>
